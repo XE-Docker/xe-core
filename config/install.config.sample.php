d24a898cd17430280139e464a44cc79aafda9598
YJSoft <YJSoft@users.noreply.github.com>
2015-06-04 13:14:32 +0900

    install.config.sample.php 생성

---
+++ config/install.config.sample.php
@@ -0,0 +1,18 @@
+<?php
+$install_config = array(
+  'db_type' =>'mysqli_innodb',
+  'db_port' =>'3306',
+  'db_hostname' =>'localhost',
+  'db_userid' =>'username_here',
+  'db_password' =>'password_here',
+  'db_database' =>'database_name_here',
+  'db_table_prefix' =>'xe',
+  'use_rewrite' =>'N',
+  'time_zone' =>'+0900',
+  'email_address' =>'admin@localhost',
+  'password' =>'xe_password_here',
+  'password2' =>'xe_password_here',
+  'nick_name' =>'admin',
+  'user_id' =>'admin',
+  'lang_type' =>'ko',
+);
